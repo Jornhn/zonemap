dc573f0145a9b5a2b579966d9f1f9add3bd73daf
jorn <jorn2522@hotmail.com>
2019-05-13 09:32:25 +0200

    add profile and header/footer changes

---
+++ profile/index.php
@@ -0,0 +1,10 @@
+<?php include('../header.php') ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-9">inloggen</div>
+        <div class="col-md-3">registeren</div>
+    </div>
+</div>
+
+<?php include('../footer.php') ?>
