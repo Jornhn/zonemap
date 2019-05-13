112b3f87b9ad73aa2c458bc381302ed97d5f3101
kwkolvoort <ko.kolvoort@gmail.com>
2019-05-13 13:16:26 +0200

    Complete upload

--- header.php
+++ header.php
@@ -1,10 +1,17 @@
-<!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Zonemap</title>
-</head>
-<body>
+<!doctype html>
+<html>
+    <head>
+        <title>Zonemap | Nieuws</title>
+        
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=width, initial-scale=1">
+        
+
+        
+        <link href="https://fonts.googleapis.com/css?family=Raleway:400,600,700,800" rel="stylesheet"> 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="../assets/css/stylenieuws.css" type="text/css" rel="stylesheet">
+        <!-- Stylesheet/bootstrap, google fonts -->
+    </head>
+    <body>
     
