4b79f8b7f26dd60765b23e3b5506dce568b53765
jorrittem <j.tempels7@gmail.com>
2019-05-21 15:07:10 +0200

    Map Sprint 1
    Map pagina, style nog niet compleet.
    Google maps gebruikt als opvulling

--- map/index.php
+++ map/index.php
@@ -1,6 +1,60 @@
 <?php include('../header.php') ?>
 
-<!-- HTML CODE HIER... -->
-<!-- In header.php de style linkjes plakken en in footer.php de javascript links plakken -->
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Zonemap | Map</title>
+
+	<!-- Meta tags -->        
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=width, initial-scale=1">
+
+    <!--Style links-->        
+    <link href="stylemap.css" type="text/css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,600,700,800" rel="stylesheet"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+    <!-- Stylesheet/bootstrap, google fonts -->
+</head>
+<body>	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-9 padding-0">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m19!1m12!1m3!1d9551.304006708231!2d6.539312006640636!3d53.2388953120246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m4!3e6!4m0!4m1!2s+!5e0!3m2!1snl!2snl!4v1557688908639!5m2!1snl!2snl" width="800" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>	
+			</div>
+
+			<div class="col-md-3 padding-0">
+				<div class="mapStyleInfo">
+					<h2>Van OlstToren</h2>
+					<h4>Zernike plein 7</h4>
+					<img src="Image16.png">
+
+					<p><b>Openingstijden:</b></p>
+					<p>09:00 - 17:00</p>
+
+					<p><b>Opleidingen</b></p>
+					<p>
+					Communicatie en Multimedia Design
+					<br>
+					Communicatie
+					<br>
+					Business
+					</p>
+
+					<p><b>Voorzieningen</b></p>
+					<p>
+					Toiletten
+					<br>
+					Eetvoorzieningen
+					</p>
+
+				</div>
+				
+			</div>
+		</div>
+	</div> 
+</body>
+</html>
 
 <?php include('../footer.php') ?>
