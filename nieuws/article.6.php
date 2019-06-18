8b47ac4de284137e071fa68fdcaa82a99571cc7d
kwkolvoort <ko.kolvoort@gmail.com>
2019-06-18 10:59:59 +0200

    Toevoeging nieuw artikel

---
+++ nieuws/article.6.php
@@ -0,0 +1,41 @@
+<?php include('../header.php') ?>
+
+<div class="row">
+
+<div class="col-12">
+    
+    <div class="toptext">
+    <a href="index.php"><div class="back">Vorige</div></a>
+    <img src="../assets/images/avatar.png" class="avatarsm"><h1 class="toptext1">RUG presenteert plannen uitbreiding Aletta Jacobshal op Zernike</h1>
+    </div>
+
+    <div class="writer">
+    <img src="../assets/images/avatar.png" class="avatar"><h4 class="newsdate1">12-07-2018</h4>
+    </div>
+
+    <div class="artikeltext1">
+        <p class="ondertiteltextsm">De wens van faculteiten en docenten van de universiteit is helder: meer tentamens (digitaal) kunnen afnemen tijdens de tentamenperiode. De bestaande Aletta Jacobshal op de Zernike Campus heeft daarvoor op dit moment te weinig capaciteit. Daarom heeft de Rijksuniversiteit Groningen (RUG) samen met architectenbureau Onix NL uit Groningen een plan gemaakt voor een uitbreiding met meer college- en tentamenruimten, een extra foyer/wachtruimte, studielandschap en meer fietsenstallingen. BAM Bouw en Techniek is de aannemer die het project gaat uitvoeren.</p>
+    </div>
+
+    <div class="headerimg">
+        <img src="../assets/images/ajnieuwgr.png" class="headerimage">
+    </div>
+
+    <div class="artikeltext">
+
+        <p class="ondertiteltext">De wens van faculteiten en docenten van de universiteit is helder: meer tentamens (digitaal) kunnen afnemen tijdens de tentamenperiode. De bestaande Aletta Jacobshal op de Zernike Campus heeft daarvoor op dit moment te weinig capaciteit. Daarom heeft de Rijksuniversiteit Groningen (RUG) samen met architectenbureau Onix NL uit Groningen een plan gemaakt voor een uitbreiding met meer college- en tentamenruimten, een extra foyer/wachtruimte, studielandschap en meer fietsenstallingen. BAM Bouw en Techniek is de aannemer die het project gaat uitvoeren.</p>
+        <p class="hoofdtext">Het gebouw wordt aangesloten op een WKO-installatie (Warmte Koude Opslag) voor het duurzaam verwarmen en koelen. Binnen het project worden er bovendien 576 zonnepanelen op het dak van het complex aangebracht en krijgt de foyer een betere klimaatbeheersing. Naar verwachting start de bouw eind 2018 en zal de ingebruikname rond begin 2020 zijn.
+        <br><br><b>Verdubbeling</b><br>In de Aletta Jacobshal komen er twee nieuwe collegezalen bij, één zaal van 200 zitplaatsen en één zaal van 450 zitplaatsen, en een zaal met 590 (digitale) tentamenplaatsen. De uitbreiding levert zo’n 5.400 m2 extra (bruto) vloeroppervlak op, nagenoeg een verdubbeling van de huidige situatie. De nieuwbouw wordt aan de zuidoostzijde tegen de bestaande hal aangebouwd, deels in de vijver. De fietsenstalling krijgt aan de achterzijde een uitbreiding en gaat van 1.060 naar ca. 1.550 plaatsen.<br><br>
+        Daarnaast komt er aanzienlijk meer wachtruimte voor studenten door een extra foyer toe te voegen. De huidige entree zal daardoor meer als uitgang worden gebruikt. Door een beter klimaatbeheersingssysteem zal het binnenklimaat in de bestaande foyer bovendien sterk verbeteren. Ten slotte is er in het ontwerp ruimte voor studieplekken gecreëerd.<br><br>
+        <b>Beperken overlast tijdens tentamenperiode</b><br>De grote uitdaging in dit project is uiteraard het beperken van overlast als gevolg van de bouwwerkzaamheden. Een belangrijk selectiecriterium voor de aannemer tijdens de aanbesteding was dan ook het slim plannen van bouwwerkzaamheden rondom tentamenperiodes. Meer details over de planning en maatregelen volgen later.<br>
+        <br><br><i>Bron artikel: <a href="https://www.rug.nl/groundbreakingwork/projects/alettajacobshal/news/rug-presenteert-plannen-uitbreiding-aletta-jacobshal">www.rug.nl</a></i>
+        </p>
+
+    </div>
+
+
+</div>
+
+</div>
+
+<?php include('../footer.php') ?>
